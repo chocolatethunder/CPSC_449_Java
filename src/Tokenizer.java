7653f1070bac7fdbee8fd7c8c88eb9cc0f28198e
Kowther Hassan <kowtherh@gmail.com>
2017-02-21 23:38:34 -0700

    Tokenizer class - first pass
    Takes a string and creates tokens

---
+++ src/Tokenizer.java
@@ -0,0 +1,57 @@
+import java.util.ArrayList;
+
+public class Tokenizer {
+	String input;
+	ArrayList<String> tokens;
+
+
+
+	// constructor takes in an input string to be tokenized
+	public Tokenizer ( String input ) {
+		
+		this.input = input;
+		makeTokenList();		// makes token list
+		
+	}
+	
+	
+	public void makeTokenList() {
+		tokens = new ArrayList<String>();
+		String expression = "";
+		
+		for (int i = 0; i < input.length(); i++) {
+			
+			char j = input.charAt( i );
+			switch ( j ) {
+				case '(':
+					
+					tokens.add(j + "");				// converted into string
+					break;
+				case ' ':
+					if (expression.length() > 0)
+						tokens.add(expression);		// add token if expression is not empty
+					expression = "";				// reset expression
+					break;
+				case ')':
+					tokens.add(expression);			// add expression before ')'
+					expression = "";				// reset expression
+					tokens.add(j + "");				// converted into string
+					break;
+				default:
+					expression += (j + "");			// add character to expression
+					break;
+			}	
+		}
+	}
+	
+	// getter for tokens
+	public ArrayList<String> getTokens() {
+		return tokens;
+	}
+	
+	public static void main(String[] args) {
+	        
+			Tokenizer tokenizer = new Tokenizer( "(add (add 3 2) 2)" );
+	        System.out.println(tokenizer.getTokens());
+	   }
+}
