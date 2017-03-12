32392fd4cb22ca315beb3f5de934224ed09d9bb3
Tit-tytant <saurabh.tomar@gmail.com>
2017-03-12 01:04:46 -0700

    Matched filename to class name

--- src/parser/exceptions/UnrecognizedQualifier.java
+++ src/parser/exceptions/UnrecognizedQualifier.java
@@ -6,12 +6,12 @@ import java.util.*;
 public class UnrecognizedQualifier extends ExceptionHandler {
     
     public UnrecognizedQualifier(String qualifier) {
-        System.err.println("Unrecognized qualifier: --" + qualifier + ".");
+        System.err.println("Unrecognized qualifier: " + qualifier + ".");
         ExceptionHandler.printSynopsis(false);
         System.exit(-1);
     }
     
-    public UnrecognizedQualifier(String letter, String source) {        
+    public UnrecognizedQualifier(char letter, String source) {        
         System.err.println("Unrecognized qualifier '" + letter + "' in '" + source + "'.");
         ExceptionHandler.printSynopsis(false);
         System.exit(-1);
