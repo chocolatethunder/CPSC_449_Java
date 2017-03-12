ba5eac325a2030cc7907c30fcd832ade9246f499
Tit-tytant <saurabh.tomar@gmail.com>
2017-03-12 01:05:40 -0700

    Needs empty contructor for when no file is provided

--- src/parser/exceptions/ErrorLoadingJarFile.java
+++ src/parser/exceptions/ErrorLoadingJarFile.java
@@ -5,6 +5,11 @@ import java.util.*;
 // Fatal error thrown when the jar file could not be loaded
 public class ErrorLoadingJarFile extends ExceptionHandler {
     
+    public ErrorLoadingJarFile() {
+        System.err.println("Could not load jar file: None given.");
+        System.exit(-5);
+    }
+    
     public ErrorLoadingJarFile(String filename) {
         System.err.println("Could not load jar file: " + filename);
         System.exit(-5);
