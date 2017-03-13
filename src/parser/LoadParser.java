36ae702d67371d646c508f23c25149234e4113bb
Tit-tytant <saurabh.tomar@gmail.com>
2017-03-13 02:26:35 -0600

    New command line arguments validator
    The old code had massive logic errors. This new code seeks to address
    that issue.

--- src/parser/LoadParser.java
+++ src/parser/LoadParser.java
@@ -6,7 +6,6 @@ import java.lang.*;
 import java.text.*;
 import java.net.*;
 
-import parser.exceptions.*;
 
 /**
  * Checks the command-line input for various conditions prior to running the Parser
@@ -14,153 +13,84 @@ import parser.exceptions.*;
  */
 public class LoadParser {
 	
+	private List<String> arguments	= new ArrayList<String>();
+	private List<String> flags		= new ArrayList<String>();
+	private List<String> longFlags 	= new ArrayList<String>();
+	private String[] cmdLnArgS;
+	
 	/**
 	 * @param args - represents command-line arguments
 	 */
 	public LoadParser(String[] args) throws Exception {
 		
+		// make a local copy of the command line arguments
+		cmdLnArgS = args.clone();
 		
-	   // Functional Requirement 1.1 and 1.2
-	   
-	   // flag represents what qualifier letter was chosen
-	   int i = 0, j = 0, k = 0;		
-	   char flag;
-	   String arg;
-	   String jarFile;
-	   
-	   
-	   // Checks the command-line input for the length of the input as long as it begins with "-" (proper format)
-	   while (i < args.length && args[i].startsWith("-")) {
-		   
-		   // current argument
-		   arg = args[i];
-		   
-		   // check all the FAT arguments first
-			if (arg.startsWith("--")) {				  
-				
-				// Check to see if help is called without any arguments
-				if (arg.equals("--help")) {
-					
-					// this takes care of the { }* where there can be more than one --help punched in
-					while(i < args.length && ( args[i].equals("--help") || args[i].equals("--verbose") )) {
-						i++;
-					}
-					
-					// if there no extra ARGS left then print synopsis else throw <Exception>				
-					if ((i+1) > args.length) {
-						ExceptionHandler.printSynopsis(true);	
-					} else {
-						throw new UnexpectedHelpQualifier();		
-					}
-					
-				// check if verbose mode is set
-				} else if (arg.equals("--verbose")) { 
-					// Activate verbose mode
-					ExceptionHandler.verboseOn();
-					
-					// this takes care of the { }* where there can be more than one --verbose punched in
-					while(i < args.length && args[i].equals("--verbose")) {
-						i++;
-					}
-					
-					// check if the next argument is actually a jar file otherwise throw <Exception>
-					if ((i+1) > args.length) {
-					   // pass to jarLoader and launch program *********
-					   jarFile = args[i++];
-					} else {
-						throw new NoJarFileGiven();
-					}
-					
+		// Functional Requirement 1.1 and 1.2
+		int i = 0, j = 0, k = 0;		
+		char flag;
+		String cmdLnArg;
+		String jarFile;		
+		
+		
+		
+		// Collect all the args, flags, and longFlags		
+		while (i < this.cmdLnArgS.length) {
+			
+			cmdLnArg = this.cmdLnArgS[i];
+			
+			// collect --flags
+			if (cmdLnArg.startsWith("--")) {				
+				if (cmdLnArg.length() > 2) {					
+					longFlags.add(cmdLnArg);					
 				} else {
-					// default is an <error>	
-					// Invalid FAT qualifier entered
-					throw new UnrecognizedQualifier(arg);
+					throw new UnrecognizedQualifier(cmdLnArg);
 				}
+			// collect -flag
+			} else if (cmdLnArg.startsWith("-")) {				
+				if (cmdLnArg.length() > 1) {
+					flags.add(cmdLnArg);
+				} else {
+					throw new UnrecognizedQualifier(' ', cmdLnArg);
+				}
+			// collect arguments
+			} else {
+				arguments.add(cmdLnArg);
+			}
+			
+			i++;			
+		}
 		
-		   // Checking short qualifiers (input starts with "-", but not "--")
-		   } else {				  
-			   
-			   // check all the flags 
-			   for (j = 1; j < arg.length(); j++) {					  
-				   
-					flag = arg.charAt(j);
-					
-					switch (flag) {
-					   
-						// help
-						case 'h':
-						case '?':
-						
-							// this takes care of the { }* where there can be more than one -h or -? punched in
-							while(j < arg.length() && (arg.charAt(j) == 'h' || arg.charAt(j) == '?' || arg.charAt(j) == 'v')) {
-								j++;
-							}
-							
-							// if there no extra FLAGS left then print synopsis else throw <Exception>							
-							if ((j+1) > (arg.length())) {
-								ExceptionHandler.printSynopsis(true);
-							} else {
-								throw new UnexpectedHelpQualifier();
-							}							
-							break;
-							 
-						// verbose flag
-						case 'v':
-							// this takes care of the { }* where there can be more than one -v punched in
-							while(j < arg.length() && flag == 'v') {
-								j++;
-							}
-
-							// check if the next argument is actually a jar file
-							if ((i+1) > args.length) {
-							   // pass to jarLoader and launch program *********
-							   jarFile = args[i++];
-							} else {
-								throw new NoJarFileGiven();
-							}
-							break;
-						   
-						// unknown flag <error>
-						// If the short qualifier is invalid
-						default:		
-							throw new UnrecognizedQualifier(flag, arg);
-					}
-			   }
-		   }
-		   i++;
-	   }
+		// remove duplicate flags and long flags
+		this.removeDuplicateFlags();
 		
-	   // Functional Requirement 1.3
-	   if (args.length == 0) {
-			ExceptionHandler.printSynopsis(false);
-	   }
-	   
-	   // Enter some of the more finer checks
-	   // Like what?
-	   
-	   // Check to see if there are at most 2 arguments given
-	   try {
-		   this.checkNumArguments(args);
-	   } catch (Exception e) { }	   
+		// Then do checks in order of priority. Order matters here.
+		
+		// Run flag compatibility (i.e. can some flags be allowed with some others)
+		
+		// Check if there are only at most 2 arguments
+		this.checkNumOfArguments();
+		
+		// Functional Requirement 1.3: No qualifiers or arguments
+		this.checkForNoArguments();
+		
 	}
 	
-	/**
-	 * @param args - represents the command-line arguments provided by the user
-	 */
-	public void checkNumArguments(String[] args) throws MoreThanTwoCommandsGiven {
-		
-		// Error checking		
-		int numArguments = 0;
-
-		for (String arg : args) {
-		   if (arg.charAt(0) != '-') {
-			   numArguments++;
-		   }
-		}
-		
-		// too many arguments throw <Exception>
-		if (numArguments > 2) {
-		   throw new MoreThanTwoCommandsGiven();
+	private void removeDuplicateFlags() {		
+		this.flags 		= new ArrayList<String>(new LinkedHashSet<String>(this.flags));
+		this.longFlags 	= new ArrayList<String>(new LinkedHashSet<String>(this.longFlags));	
+	}
+	
+	private void checkNumOfArguments() throws MoreThanTwoCommandsGiven {
+		if (this.arguments.size() > 2) {
+			throw new MoreThanTwoCommandsGiven();
 		}
 	}
+	
+	private void checkForNoArguments() {
+		if (this.cmdLnArgS.length == 0) {
+			ExceptionHandler.printSynopsis(false);
+		}
+	}
+
 }
