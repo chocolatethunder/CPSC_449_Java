a8628d5e60bc9141bd765a332ab4775987d08e75
Tit-tytant <saurabh.tomar@gmail.com>
2017-03-15 00:27:45 -0600

    Token incorporated
    Note to whoever made this. Please declare your methods private public
    protected properly. This can cause security issue.

---
+++ src/parser/parserengine/Token.java
@@ -0,0 +1,90 @@
+package parser;
+
+public class Token {
+	
+	// name of Token
+	private String name;
+	// Type of token. Can be Integer, Float, String, Char	
+	private Class type;
+	// Type of token expressed as a string. Can be int, float, string, identifier, openBracket, closedBracket
+	private String stringType;
+	// index in ArrayList. Can be used to for tracing error
+	private int index;
+	
+	/**
+	 * constructor for token. Sets the name and type
+	 * @param name
+	 * @param type
+	 */
+	public Token (String name, Class type, String stringType, int index) {
+		this.setName(name);
+		this.setType(type);
+		this.setStringType(stringType);
+		this.setIndex(index);		
+	}
+	
+	/**
+	 * getter for name
+	 * @return
+	 */
+	public String getName() {
+		return this.name;
+	}
+
+	/**
+	 * setter for name
+	 * @param name
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * getter for type
+	 * @return
+	 */
+	public Class getType() {
+		return this.type;
+	}
+
+	/**
+	 * setter for type
+	 * @param type
+	 */
+	public void setType(Class type) {
+		this.type = type;
+	}	
+	
+	/**
+	 * getter for index
+	 * @return
+	 */
+	public int getIndex() {
+		return this.index;
+	}
+
+	/**
+	 * setter for index
+	 * @param index
+	 */
+	public void setIndex(int index) {
+		this.index = index;
+	}
+
+	/**
+	* getter for stringType
+	* @return
+	*/
+	public String getStringType() {
+		return this.stringType;
+	}
+	 
+	 /**
+	  * setter for stringType
+	  * @param stringType
+	  */
+	public void setStringType(String stringType) {
+		this.stringType = stringType;
+	}
+
+}
