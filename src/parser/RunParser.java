6bd815c3a3d9f49cdef813e5e4a026aba1f8d471
Tit-tytant <saurabh.tomar@gmail.com>
2017-03-14 19:37:56 -0600

    Program menu added
    Functional Requirements 2 and 3 complete.

--- src/parser/RunParser.java
+++ src/parser/RunParser.java
@@ -6,21 +6,84 @@ import java.lang.*;
 import java.text.*;
 import java.net.*;
 
+import static parser.Utilities.*;
+
 
 /**
  * 
  * 
  *
  */
-public class RunParser {    
+public class RunParser {
+
+	private boolean quit = false;
+	private String input = "";
+	    
+	public RunParser(JarFileLoader jarLoad) {
+		
+		Scanner reader = new Scanner(System.in);
+		
+		// Print start message
+		this.printStartup();
+
+		while (!quit) {
+			
+			// Print prompt
+			System.out.print("> ");
+			
+			// Capture input
+			input = reader.next();
+			
+			// Process
+			
+			// Trim leading and trailing spaces
+			input.trim();
+			
+			if (input.length() == 1) {
+				
+				char meta = input.charAt(0);
+				
+				// Process meta commands
+				switch(meta) {
+					
+					// Quit
+					case 'q':
+					this.quit = true;
+					System.out.println("bye.");
+					break;
+					
+					// Verbose
+					case 'v':
+					ExceptionHandler.toggleVerbose();
+					if (ExceptionHandler.isVerbose()) {
+						System.out.println("Verbose on");
+					} else {
+						System.out.println("Verbose off");
+					}
+					break;
+					
+					// Function List
+					case 'f':
+					printFunctionList(jarLoad);
+					break;
+					
+					// Pring Help
+					case '?':
+					this.printStartup();
+					break;
+					
+					default:
+					break;
+					
+				}
+				
+			}			
+			
+		}
+
+	}
     
-    public RunParser() {
-       
-       RunParser.printStartup();
-       
-    }
-    
-    private static void printStartup(){
+	private void printStartup(){
 		System.out.println(
 		"q           : Quit the program.\n" +
 		"v           : Toggle verbose mode (stack traces).\n" +
@@ -29,8 +92,7 @@ public class RunParser {
 		"<expression>: Evaluate the expression.\n" +
 		"Expressions can be integers, floats, strings (surrounded in double quotes) or function calls of the form '(identifier {expression}*)'."
 		);
-	}
-    
+	}    
     
     
 }
