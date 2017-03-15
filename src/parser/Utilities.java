7a64feb666ba5958e217f4c5c172758bc0cb0e5e
Tit-tytant <saurabh.tomar@gmail.com>
2017-03-14 19:37:20 -0600

    Utilities Class added
    Credit: Alan

---
+++ src/parser/Utilities.java
@@ -0,0 +1,42 @@
+package parser;
+
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+import java.lang.reflect.*;
+import java.text.*;
+import java.net.*;
+
+public class Utilities {
+	
+	// The following method will print out every function, its parameters' type, and return type
+    public static void printFunctionList(Object o) {
+    	String functionList = "";
+    	
+    	Class cls = o.getClass();
+    	Method[] methods = cls.getMethods();
+    	String methodName;
+    	
+    	Class[] parameters = null;
+    	String parameter;
+    	String returnType;
+    	
+    	for (int i = 0; i < methods.length; i++) {
+    		methodName = methods[i].getName();
+    		parameters = methods[i].getParameterTypes();
+    		returnType = methods[i].getReturnType().getName();
+    		
+    		functionList += "("  + methodName;
+    		
+			for (int j = 0; j < parameters.length; j++) {
+    			parameter = parameters[j].getName();
+    			functionList += " " + parameter;
+    		}
+			
+    		functionList += ") : " + returnType + "\n";
+    	}
+    	
+    	System.out.println(functionList);
+    }
+
+}
