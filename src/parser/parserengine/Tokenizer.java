a12d40297eb095472fb8f3bc1ad8e95512265e7e
Tit-tytant <saurabh.tomar@gmail.com>
2017-03-15 10:34:37 -0600

    Tokenizer added
    This class is responsible for creating tokens our of a string. (@tylam)

--- src/parser/parserengine/Tokenizer.java
+++ src/parser/parserengine/Tokenizer.java
@@ -3,18 +3,25 @@ package parser;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
 
+import static parser.Utilities.*;
+
 // this file needs to be re-written. what is this code bruh? It hurt my brain!
 
 public class Tokenizer {
 	
 	String input = "";
-	ArrayList<Token> tokens = new ArrayList<Token>;
+	ArrayList<Token> tokens = new ArrayList<Token>();
+	int tokenPosition = 0;
 	
 	public Tokenizer (String expression) {
 		
 		// Debug
 		System.out.println(expression);
 		
+	}
+	
+	public ArrayList<Token> getTokens() {
+		return this.tokens;
 	}
 
 }
