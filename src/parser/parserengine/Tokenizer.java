d086923c25cc6467610054b796f74de7c1dcb5eb
Tit-tytant <saurabh.tomar@gmail.com>
2017-03-15 01:22:37 -0600

    Re-working Tokenizer file.
    I have a beef to pick with who ever wrote the original version of this
    file.

---
+++ src/parser/parserengine/Tokenizer.java
@@ -0,0 +1,20 @@
+package parser;
+
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+
+// this file needs to be re-written. what is this code bruh? It hurt my brain!
+
+public class Tokenizer {
+	
+	String input = "";
+	ArrayList<Token> tokens = new ArrayList<Token>;
+	
+	public Tokenizer (String expression) {
+		
+		// Debug
+		System.out.println(expression);
+		
+	}
+
+}
