6aaf73dc4788fc4299531c0b39a952e4c48f61bc
Kaylee Stelter <kaylee.stelter@ucalgary.ca>
2017-03-15 22:31:54 -0600

    Javadoc

--- src/parser/Main.java
+++ src/parser/Main.java
@@ -6,7 +6,9 @@ import java.lang.*;
 import java.text.*;
 import java.net.*;
 
-
+/**
+ * Attempts to start the program (i.e. load the parser)
+ */
 public class Main {    
     
 	public static void main (String[] args) {
